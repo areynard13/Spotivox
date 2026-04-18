ac96cb11b3b8c17d631f0f01cecb36e25917358a
Adrien Reynard <reynard.adrien.08@gmail.com>
2026-04-18 17:02:04 +0200

    feat: add basic controls tools

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import os
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+from smolagents import tool
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,3 +18,56 @@ try:
     print(f"Connect as : {user_info['display_name']}")
 except Exception as e:
     print(f"Configuration error: {e}")
+
+# ————— Basic Controls ——————————————————————————
+@tool
+def  play_track(song_name: str) -> bool:
+    """
+    Searches for a specific song and starts playback
+    Args:
+        song_name: The name of the song to play
+    Return: bool - the success of the action
+    """
+    results = sp.search(q=song_name, limit=1, type='track')
+    if results['tracks']['items']:
+        uri = results['tracks']['items'][0]['uri']
+        name = results['tracks']['items'][0]['name']
+        sp.start_playback(uris=[uri])
+        return True
+    return False
+
+@tool
+def pause_playback() -> bool:
+    """
+    Pauses the current music playback
+    Return: bool - the success of the action
+    """
+    sp.pause_playback()
+    return True
+
+@tool
+def resume_playback() -> bool:
+    """
+    Resumes the current music playback.
+    Return: bool - the success of the action
+    """
+    sp.start_playback()
+    return True
+
+@tool
+def skip_next() -> bool:
+    """
+    Skips to the next track in the queue.
+    Return: bool - the success of the action
+    """
+    sp.next_track()
+    return True
+
+@tool
+def skip_previous() -> bool:
+    """
+    Skips to the previous track.
+    Return: bool - the success of the action
+    """
+    sp.previous_track()
+    return True
